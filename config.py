c480314f93f43a0de186ba824db66256c850dbdb
samuelsih <samuel.205025@mhs.its.ac.id>
2023-08-08 20:58:50 +0700

    breaking: move to lambda

--- config.py
+++ config.py
@@ -1,19 +1,9 @@
-import os
 import firebase_admin
 from firebase_admin import credentials
-from dotenv import load_dotenv
-
-env = os.getenv(key="ENVIRONMENT", default="DEV")
-_is_production = True
-
-if env == "DEV":
-    _is_production = False
-    load_dotenv()
 
 
 class Config:
     def __init__(self):
-        self.is_production = _is_production
         self.fb_app = self.__firebase_app()
 
     def __firebase_app(self) -> firebase_admin.App:
